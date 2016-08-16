e3a21041853d3f35cc3101c3d112b9f481bc8c40
Matt Mowris <matt.mowris@gmail.com>
2016-08-16 13:05:59 -0700

    v7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,7 +45,7 @@ public class Main {
 
         ArrayList<String> output = new ArrayList<String>();
         while (rs.next()) {
-          output.add( "Read from DB: " + rs.getString(1));
+          output.add( "Read from DB: " + rs.getString(1) + rs.getString(2) + rs.getString(3) );
           //output.add( "Read from DB: " + rs.getTimestamp("tick"));
         }
 
