2c04c76edf46a15159ae9c6a46e3dd0b05db590d
Matt Mowris <matt.mowris@gmail.com>
2016-08-16 12:59:09 -0700

    v6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,8 +34,9 @@ public class Main {
       Map<String, Object> attributes = new HashMap<>();
       try {
         connection = DatabaseUrl.extract().getConnection();
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        
+        //Statement stmt = connection.createStatement();
+        //stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
         //stmt.executeUpdate("SELECT * FROM Account");
         //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
         ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.account");
