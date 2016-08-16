b4c2a23c734deef6e0a6b7a9e4294fbf1d1c8806
Matt Mowris <matt.mowris@gmail.com>
2016-08-16 11:31:47 -0700

    v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,34 @@ import static spark.Spark.get;
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        
+        Connection connection = getConnection();
+        
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+        }
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-  public static void main(String[] args) {
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+}
+
+  /*public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
@@ -57,4 +83,4 @@ public class Main {
 
   }
 
-}
+}*/
